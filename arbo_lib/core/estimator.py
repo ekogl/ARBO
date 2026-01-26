e6995acb67b064d6ba5e7476c01fb16e82768809
Elias Kogler <elias.kogler@student.uibk.ac.at>
2026-01-26 14:06:51 +0100

    implemented estimator logic, fix bug in residual

--- arbo_lib/core/estimator.py
+++ arbo_lib/core/estimator.py
@@ -1,19 +1,144 @@
-# TODO: implement estimator logic, will use amdahl and residual
+import numpy as np
+from typing import List, Dict, Optional
+
+from arbo_lib.db.store import ArboState
+from arbo_lib.core.amdahl import AmdahlUtils
+from arbo_lib.core.residual import ResidualModel
+from arbo_lib.core.exceptions import TaskNotFoundError, TaskAlreadyExistsError
 
 class ArboEstimator:
     def __init__(self):
-        pass
+        self.store = ArboState()
+        self.residual_model = ResidualModel()
 
-    def predict_total_time(self):
+    def predict(self, task_name: str, gamma: float, cluster_load: float, max_time_slo: Optional[float] = None) -> int:
         """
-        Combines AmdahlUtils.calculate_time + ResidualModel.predict
+        Main Optimization loop, return optimal 's'
         :return:
         """
-        raise NotImplemented
+        params = self.store.get_task_model(task_name)
 
-    def optimize(self):
+        # cold start
+        if not params:
+            print(f"Cold Start for task {task_name}, forcing s=1")
+            return 1
+
+        # TODO: check if sample_count = 1 -> baseline done, might need additional mildly parallel run to estimate p
+
+        # train GP on last 50 executions
+        history = self.store.get_history(task_name, limit=50)
+        self.residual_model.train(history)
+
+        # TODO: smart way to figure out search space [1, x] -> maybe based on p?
+        best_s = 1
+        best_score = float("inf")
+
+        candidates_s = np.arange(1, 21)
+
+        residuals = self.residual_model.predict(candidates_s, gamma, cluster_load)
+
+        for i, s in enumerate(candidates_s):
+            # calculate theoretical time
+            t_amdahl = AmdahlUtils.calculate_theoretical_time(s=s, t_base=params["t_base_1"], c_startup=params["c_startup"], gamma=gamma, p=params["p_obs"])
+
+            t_total = t_amdahl + residuals[i]
+
+            # time constraint
+            if max_time_slo and t_total > max_time_slo:
+                continue
+
+            # TODO: implement proper cost function
+            cost = t_total * s
+
+            if cost < best_score:
+                best_score = cost
+                best_s = s
+
+        return int(best_s)
+
+
+    def feedback(self, task_name: str, s: int, gamma: float, cluster_load: float, t_actual: float) -> None:
         """
-        Optimization Loop (Phase 2 of proposed algorithm)
+        Learning loop: updates parameters and saves execution
         :return:
         """
-        raise NotImplemented
+        params = self.store.get_task_model(task_name)
+
+        # cold start
+        if not params:
+            print(f"Initializing {task_name}")
+
+            try:
+                self.store.initialize_task(task_name=task_name, t_base=t_actual)
+
+                # TODO: implement proper cost function
+                cost = t_actual * 1
+                run_data = self._pack_run_data(task_name, s, gamma, cluster_load, t_actual, residual=0, cost=cost)
+                self.store.update_model(task_name, new_p=1, run_data=run_data)
+                return
+            except TaskAlreadyExistsError:
+                # TODO: properly handle exception
+                print(f"Task {task_name} already exists in DB")
+                params = self.store.get_task_model(task_name)
+
+
+            # infer p
+            p_current = AmdahlUtils.calculate_current_p(
+                s=s,
+                t_actual=t_actual,
+                c_startup=params["c_startup"],
+                t_base=params["t_base_1"],
+                gamma=gamma
+            )
+
+            # update p
+            new_p = AmdahlUtils.update_p_moving_average(
+                old_p=params["p_obs"],
+                current_p=p_current,
+                alpha=params["alpha"]
+            )
+
+            # calculate residual
+            t_theory = AmdahlUtils.calculate_theoretical_time(
+                s=s,
+                t_base=params["t_base_1"],
+                c_startup=params["c_startup"],
+                gamma=gamma,
+                p=new_p
+            )
+            residual = t_actual - t_theory
+
+            # TODO: update cost metric
+            cost = t_actual * s
+            run_data = self._pack_run_data(task_name, s, gamma, cluster_load, t_actual, residual, cost)
+
+            try:
+                self.store.update_model(task_name, new_p=new_p, run_data=run_data)
+            except TaskNotFoundError:
+                print(f"Task {task_name} not found in DB")
+
+
+
+
+
+    def _pack_run_data(self, task, s, gamma, cluster_load, time, residual, cost):
+        """
+        Helper to pack dictionary for storing
+        :param task:
+        :param s:
+        :param gamma:
+        :param cluster_load:
+        :param time:
+        :param residual:
+        :param cost:
+        :return:
+        """
+        return {
+            "task_name": task,
+            "s": s,
+            "gamma": gamma,
+            "cluster_load": cluster_load,
+            "total_duration": time,
+            "residual": residual,
+            "cost_metric": cost
+        }
--- arbo_lib/core/residual.py
+++ arbo_lib/core/residual.py
@@ -1,20 +1,51 @@
-# TODO: implement logic surrounding the residual
+import numpy as np
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
+from typing import List, Dict
 
 class ResidualModel:
+    """
+    Gaussiona Process to learn the residual (error) of Amdahl's Law cannot explain
+    Input (X): [Parallelism (s), Input Ścale (gamma), Cluster Load]
+    Output (y): Residual time
+    """
 
     def __init__(self):
-        pass
+        kernel = C(1.0, (1e-3, 1e3)) * RBF([10, 1, 10], (1e-2, 1e2))
+        self.model = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=2, alpha=1.0)
+        self.is_trained = False
 
-    def train(self):
+    def train(self, history_rows: List[Dict]) -> None:
         """
+        Trains GP on the historical data
         :return:
         """
-        raise NotImplemented
+        if not history_rows:
+            self.is_trained = False
+            return
 
+        X = []
+        y = []
+        for row in history_rows:
+            X.append([row["parallelism"], row["input_scale_factor"], row["cluster_load"]])
+            y.append(row["residual"])
 
-    def predict(self):
+        self.model.fit(np.array(X), np.array(y))
+        self.is_trained = True
+
+    def predict(self, s_candidates: np.ndarray, gamma: float, cluster_load: float) -> np.ndarray:
         """
-
+        Predicts residual for a list of candiate 's' values
         :return:
         """
-        raise NotImplemented
+        if not self.is_trained:
+            return np.zeros(len(s_candidates))  # if no history, assume zero residuals
+
+        X_pred = np.column_stack([
+            s_candidates,
+            np.full(len(s_candidates), gamma),
+            np.full(len(s_candidates), cluster_load)
+        ])
+
+        return self.model.predict(X_pred)
+
